6789f5a4a0417ece656add5d987b1228e0fe8d5b
Andreas Eriksson <andreas.eriksson@jayway.com>
2015-10-03 00:58:51 +0200

    Removed warnings when starting server

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ var store = require('./store');
 var bodyParser = require('body-parser');
 var app = express();
 
-//app.use( bodyParser.json() );       // to support JSON-encoded bodies
-//app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//  extended: true
-//}));
-app.use(bodyParser());
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
